8a5de8e9831d3aefa08b44a6bb2d498d1d1a9d4b
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-06 17:03:06 +0100

    pari o dispari#1

--- main.js
+++ main.js
@@ -9,19 +9,33 @@ let risultato
 }
 
 let userChose = prompt("scegli pari o dispari")
-if (userChose = pari){
-    computerChose = dispari
+if (userChose = "pari"){
+    computerChose = "dispari"
 } else {
-    computerChose = pari
+    computerChose = "pari"
 }
 
-if(scelta != "pari" && scelta != "dispari") {
+if(userChose != "pari" && userChose != "dispari") {
     alert("Errore. Abbiamo scelto per te: punterai sui pari");
-    scelta = "pari";
+    userChose= "pari";
 }
+console.log(userChose)
 
 let inputUser = prompt("scegli un numero da 1 a 5")
 if (inputUser < 1 || inputUser > 5 || inputUser === "") {
-    alert("Please choose a number from (1-5)");
-    
+    alert("Errore. Abbiamo scelto per te: hai scelto 3 ");
+    inputUser = 3
+}
+console.log(inputUser)
+
+inputComputer = Math.floor(Math.random()*5)+1
+console.log(inputComputer)
+
+let risultato =  pariOdispari(inputUser + inputComputer);
+console.log(risultato)
+
+if (risultato = userChose){
+    alert("hai vintooo")
+} else  {
+    alert("hai persooo")
 }
