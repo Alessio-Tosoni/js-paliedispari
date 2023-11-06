21a67473bb7a24d6a4cafba46bf6ebf7f6913cfd
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-06 17:45:05 +0100

    termina pari o dispari e palindromi

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let risultato
 }
 
 let userChose = prompt("scegli pari o dispari")
-if (userChose = "pari"){
+if (userChose == "pari") {
     computerChose = "dispari"
 } else {
     computerChose = "pari"
@@ -22,6 +22,7 @@ if(userChose != "pari" && userChose != "dispari") {
 console.log(userChose)
 
 let inputUser = prompt("scegli un numero da 1 a 5")
+
 if (inputUser < 1 || inputUser > 5 || inputUser === "") {
     alert("Errore. Abbiamo scelto per te: hai scelto 3 ");
     inputUser = 3
@@ -34,8 +35,35 @@ console.log(inputComputer)
 let risultato =  pariOdispari(inputUser + inputComputer);
 console.log(risultato)
 
-if (risultato = userChose){
+if (risultato == userChose){
     alert("hai vintooo")
 } else  {
     alert("hai persooo")
 }
+
+function palindromatore(stringa) {
+
+    
+    const lunghezza = stringa.length;
+
+    
+    for (let i = 0; i < lunghezza / 2; i++) {
+
+        
+        if (stringa[i] !== stringa[lunghezza - 1 - i]) {
+            return 'non è palindromo';
+       
+        } else{
+        return 'è un palindromo'; 
+        }
+    }
+}
+
+
+const stringa= prompt('scrivi quello che vuoi verificare: ');
+
+
+const analisi = palindromatore(stringa);
+
+console.log(analisi);
+alert(analisi)
